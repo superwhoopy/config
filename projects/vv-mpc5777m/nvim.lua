df0abfb61716f13cde8cae8705a910bba9dc4262
Emmanuel Ohayon <eohayon@mail.ks.int>
2023-06-13 18:22:46 +0200

    stuff

--- projects/vv-mpc5777m/nvim.lua
+++ projects/vv-mpc5777m/nvim.lua
@@ -1,5 +1,18 @@
 print('Sourcing file ' .. vim.fn.expand('<sfile>:t'))
 
 -- set CPP options for ALE
-vim.g.ale_c_cc_options = '-std=c99 -Wall -Wextra -Wno-override-init -DAST_K2_ -D_DIAB_TOOL -Irtk-mpc5777m/bundle/include -Irtk-mpc5777m/bundle/lib/include -Irtk-mpc5777m/sources_bundle/include -Irtk-mpc5777m/sources_bundle/lib/include -Icomponent_tests/lib/kunit/include'
+vim.g.ale_c_cc_options = table.concat({
+  '-std=c99',
+  '-Wall',
+  '-Wextra',
+  '-Wno-override-init',
+  '-Wno-int-to-void-pointer-cast',
+  '-DAST_K2_',
+  '-D_DIAB_TOOL',
+  '-Irtk-mpc5777m/bundle/include',
+  '-Irtk-mpc5777m/bundle/lib/include',
+  '-Irtk-mpc5777m/sources_bundle/include',
+  '-Irtk-mpc5777m/sources_bundle/lib/include',
+  '-Icomponent_tests/lib/kunit/include',
+}, ' ')
 
